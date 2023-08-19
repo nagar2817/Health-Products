0cbcaafdbaea9c4e9db0260ab1276bdf6fd70594
nagar2817 <nagar.2@iitj.ac.in>
2023-08-19 12:15:11 +0530

    2 files modified, 1 deleted

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,17 +6,14 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import LandingPage from './Hero';
-import ParticleBackground from './ParticleBackground.js';
 
 
 function Home() {
   return (
     <Container>
       <Typography variant="h4" component="h1" align="center" gutterBottom>
-        Welcome to our AI Healthcare Startup
+        Welcome to our AI Healthcare Shop
       </Typography>
-    <ParticleBackground />
-     
     </Container>
   );
 }
